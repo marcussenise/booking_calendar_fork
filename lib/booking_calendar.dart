b17a64eb81853db8bc86ee931b9b6631d2a66259
Marcus <marcus.senise@hotmail.com>
2023-05-23 13:01:08 -0300

    mudança pra BookingServiceModel na pasta lib principal

--- lib/booking_calendar.dart
+++ lib/booking_calendar.dart
@@ -1,5 +1,5 @@
 library booking_calendar;
 
 export 'src/core/booking_calendar.dart';
-export 'src/model/booking_service.dart';
+export 'src/model/booking_service_model.dart';
 export 'src/model/enums.dart';
---
+++ lib/src/model/booking_service_model.dart
@@ -0,0 +1,92 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+
+import 'dart:convert';
+
+class BookingServiceModel {
+  final String? key;
+  final String carro;
+  final String data;
+  final String horario;
+  DateTime bookingStart;
+  DateTime bookingEnd;
+  final int serviceDuration;
+  final String localRecarga;
+  final String? observacao;
+  final String? motivoRejeicao;
+  final String statusRecarga;
+  final String nome;
+  final String telefone;
+  final double? notaRecarga;
+  final String? antesRecargaUrl;
+  final String? depoisRecargaUrl;
+
+  BookingServiceModel({
+    this.key,
+    required this.carro,
+    required this.data,
+    required this.horario,
+    required this.bookingStart,
+    required this.bookingEnd,
+    required this.serviceDuration,
+    required this.localRecarga,
+    this.observacao,
+    this.motivoRejeicao,
+    required this.statusRecarga,
+    required this.nome,
+    required this.telefone,
+    this.notaRecarga,
+    this.antesRecargaUrl,
+    this.depoisRecargaUrl,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'key': key,
+      'carro': carro,
+      'data': data,
+      'horario': horario,
+      'bookingStart': bookingStart.millisecondsSinceEpoch,
+      'bookingEnd': bookingEnd.millisecondsSinceEpoch,
+      'localRecarga': localRecarga,
+      'observacao': observacao,
+      'motivoRejeicao': motivoRejeicao,
+      'statusRecarga': statusRecarga,
+      'nome': nome,
+      'telefone': telefone,
+      'notaRecarga': notaRecarga,
+      'antesRecargaUrl': antesRecargaUrl,
+      'depoisRecargaUrl': depoisRecargaUrl,
+      'serviceDuration' : serviceDuration,
+    };
+  }
+
+  factory BookingServiceModel.fromMap(Map<String, dynamic> map) {
+    return BookingServiceModel(
+      key: map['key'] != null ? map['key'] as String : null,
+      carro: map['carro'] as String,
+      data: map['data'] as String,
+      horario: map['horario'] as String,
+      bookingStart: DateTime.fromMillisecondsSinceEpoch(map['bookingStart'] as int),
+      bookingEnd: DateTime.fromMillisecondsSinceEpoch(map['bookingEnd'] as int),
+      localRecarga: map['localRecarga'] as String,
+      observacao: map['observacao'] != null ? map['observacao'] as String : null,
+      motivoRejeicao: map['motivoRejeicao'] != null ? map['motivoRejeicao'] as String : null,
+      statusRecarga: map['statusRecarga'] as String,
+      nome: map['nome'] as String,
+      telefone: map['telefone'] as String,
+      notaRecarga: map['notaRecarga'] != null ? map['notaRecarga'] as double : null,
+      antesRecargaUrl: map['antesRecargaUrl'] != null ? map['antesRecargaUrl'] as String : null,
+      depoisRecargaUrl: map['depoisRecargaUrl'] != null ? map['depoisRecargaUrl'] as String : null,
+      serviceDuration: map['serviceDuration'] as int,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory BookingServiceModel.fromJson(String source) => BookingServiceModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override 
+  String toString() => 'SolicitarRecargaModel(key: $key, carro: $carro, data: $data, horario: $horario, localRecarga: $localRecarga, observacao: $observacao, motivoRejeicao: $motivoRejeicao, statusRecarga: $statusRecarga, nome: $nome, telefone: $telefone, notaRecarga: $notaRecarga, antesRecargaUrl: $antesRecargaUrl, depoisRecargaUrl: $depoisRecargaUrl, serviceDuration: $serviceDuration)';
+
+}
